2c088c20ed8ddd655aff6ebd56bd6910c5db1db3
Matt Fender <mjfender@gmail.com>
2017-02-15 17:23:51 -0500

    adds commutes controller

--- app/controllers/api/v1/commutes_controller.rb
+++ app/controllers/api/v1/commutes_controller.rb
@@ -1,18 +1,27 @@
 module Api::V1
   class CommutesController < ApplicationController
     def create
-      token = request.headers['HTTP_AUTHORIZATION']
-      account_info = Auth.decode(token)
-      @profile = Profile.new(profile_params)
-      @profile.account_id = account_info['account_id']
-      if @profile.save
-        render json: @profile
+      @account = current_account
+      # this is the account object related to the post request
+      @commute = Commute.new(commute_params)
+      @commute.profile_id = @account.profile.id
+      @commute.available = true
+
+      if @commute.save
+        render json: @commute
       end
+    end
+
+    def index
+      @account = current_account
+      @commutes = Commute.where(profile_id: @account.profile.id)
+      render json: @commutes
     end
 
     private
 
     def commute_params
+      params.require(:commute).permit(:origin_id, :destination_id, :time, :nickname)
     end
 
   end
