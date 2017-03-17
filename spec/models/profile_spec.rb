54de1329ae8ad2b4a8cb6295954c67340eb9df23
joesass <dibson2013@gmail.com>
2017-03-17 15:47:14 -0400

    DRY up tests by using a before hook for Account creation

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,13 +1,23 @@
 require 'rails_helper'
 
 describe Profile do
+  before :each do
+    @john_smith_account = Account.new(email: 'john@smith.com', password: '1234')
+  end
+
   it "is valid with existing account id, name, bio, and phone number" do
-    john_smith_account = Account.new(email: 'john@smith.com', password: '1234')
-    john_smith_account.save
-    john_smith_account.profile = Profile.new(account_id: john_smith_account.id, name: 'John Smith',
+    @john_smith_account.save
+    @john_smith_account.profile = Profile.new(account_id: @john_smith_account.id, name: 'John Smith',
       bio: "I am John Smith",
       phone: "123-456-9801" )
-    expect(john_smith_account.profile).to be_valid
+    expect(@john_smith_account.profile).to be_valid
+  end
+  it "is invalid when account id is blank" do
+    john_smith_profile = Profile.new(account_id: nil, name: 'John Smith',
+      bio: "I am John Smith",
+      phone: "123-456-9801" )
+    john_smith_profile.valid?
+    expect(john_smith_profile.errors[:account]).to include("must exist")
   end
   it "is invalid when account id doesn't exist" do
     john_smith_profile = Profile.new(account_id: 1, name: 'John Smith',
@@ -17,27 +27,24 @@ describe Profile do
     expect(john_smith_profile.errors[:account]).to include("must exist")
   end
   it "is invalid without name" do
-    john_smith_account = Account.new(email: 'john@smith.com', password: '1234')
-    john_smith_account.save
-    john_smith_profile = Profile.new(account_id: john_smith_account.id,
+    @john_smith_account.save
+    john_smith_profile = Profile.new(account_id: @john_smith_account.id,
       bio: "I am John Smith",
       phone: "123-456-9801" )
     john_smith_profile.valid?
     expect(john_smith_profile.errors[:name]).to include("can't be blank")
   end
   it "is invalid without bio" do
-    john_smith_account = Account.new(email: 'john@smith.com', password: '1234')
-    john_smith_account.save
-    john_smith_profile = Profile.new(account_id: john_smith_account.id,
+    @john_smith_account.save
+    john_smith_profile = Profile.new(account_id: @john_smith_account.id,
       name: 'John Smith',
       phone: "123-456-9801" )
     john_smith_profile.valid?
     expect(john_smith_profile.errors[:bio]).to include("can't be blank")
   end
   it "is invalid without phone number" do
-    john_smith_account = Account.new(email: 'john@smith.com', password: '1234')
-    john_smith_account.save
-    john_smith_profile = Profile.new(account_id: john_smith_account.id,
+    @john_smith_account.save
+    john_smith_profile = Profile.new(account_id: @john_smith_account.id,
       name: 'John Smith',
       bio: "I am John Smith" )
     john_smith_profile.valid?
